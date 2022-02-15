33b896b1def991f6d5bf488866de83d7841ef5ec
superking84 <frankharvey88@gmail.com>
2022-02-15 17:58:20 -0500

    added props to pass down to components

--- src/components/GameContainer/GameContainer.tsx
+++ src/components/GameContainer/GameContainer.tsx
@@ -1,15 +1,24 @@
 import './GameContainer.css';
 import GuessList from "../../components/GuessList/GuessList";
-// import Keyboard from "../../components/Keyboard/Keyboard";
+import Keyboard from "../../components/Keyboard/Keyboard";
+import LetterGuessState from '../../types/LetterGuessState';
 
 interface GameContainerProps {
+    currentTurn: number;
+    numberOfTurns: number;
     guessInput: string[];
+    wordLength: number;
+    wordsGuessed: string[];
+    addLetter: (letter: string) => void;
+    getLetterGuessState(letter: string): LetterGuessState | null;
 }
-function GameContainer({ guessInput }: GameContainerProps) {
+
+function GameContainer(props: GameContainerProps) {
     return (
-        <div className='container'>
-            <GuessList guessInput={guessInput} />
-            {/* <Keyboard addLetter={this.addLetter} /> */}
+        <div>
+            <GuessList guessInput={props.guessInput} wordLength={props.wordLength} numberOfTurns={props.numberOfTurns}
+                currentTurn={props.currentTurn} wordsGuessed={props.wordsGuessed} />
+            <Keyboard addLetter={props.addLetter} getLetterGuessState={props.getLetterGuessState} />
         </div>
     );
 }
