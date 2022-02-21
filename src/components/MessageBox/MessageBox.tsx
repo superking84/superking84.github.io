5627279640f9732b37fd4901e17c3cf94ba09d00
superking84 <frankharvey88@gmail.com>
2022-02-20 19:58:43 -0500

    implemented MessageBox component

---
+++ src/components/MessageBox/MessageBox.tsx
@@ -0,0 +1,15 @@
+import './MessageBox.css';
+
+interface MessageBoxProps {
+    message: string;
+}
+
+function MessageBox(props: MessageBoxProps) {
+    return (
+        <div className="message-box">
+            <span className="message-box-message">{props.message}</span>
+        </div>
+    );
+}
+
+export default MessageBox;
