01cab9a9ff696589386951a9d01b218ec6377a6e
Frank Harvey <frankharvey88@gmail.com>
2022-03-27 10:27:29 -0400

    added routing broke Wordle out into separate page added useCallback to fix dependency issues with component functions

---
+++ src/Wordle.tsx
@@ -0,0 +1,144 @@
+import { useCallback, useEffect, useState } from "react";
+import GameContainer from "./components/GameContainer/GameContainer";
+import MessageBox from "./components/MessageBox/MessageBox";
+import Game from "./game";
+import GameState from "./types/GameState";
+import WordGuessState from "./types/WordGuessState";
+
+interface AppProps {
+    game: Game;
+}
+
+function Wordle(props: AppProps) {
+    const [guessInput, setGuessInput] = useState<string[]>([]);
+    const [message, setMessage] = useState<string | null>(null);
+    const [messageTimeout, setMessageTimeout] = useState<NodeJS.Timeout | null>(null);
+
+    const game = props.game;
+
+    const clearMessageTimeout = useCallback(function (): void {
+        if (messageTimeout !== null) {
+            clearTimeout(messageTimeout as NodeJS.Timeout);
+        }
+    }, [messageTimeout]);
+
+    const processGameTurn = useCallback(function (): void {
+        const wordGuessState: WordGuessState = game.getWordGuessState(guessInput);
+
+        if (wordGuessState === WordGuessState.Valid) {
+            const guess: string = guessInput.join('');
+            game.processGuess(guess);
+
+            let message: string | null = null;
+            switch (+game.gameState) {
+                case GameState.Lost:
+                    message = `You lost. The word was ${game.word}. Press Enter to play again.`;
+                    break;
+                case GameState.Won:
+                    message = "You win! Press Enter to play again.";
+                    break;
+                default:
+                    break;
+            }
+
+            clearMessageTimeout();
+            setGuessInput([]);
+            setMessage(message);
+            setMessageTimeout(null);
+
+        } else {
+            const message: string = wordGuessState === WordGuessState.InvalidLength ?
+                "Invalid word length" : "Not in word list";
+
+            clearMessageTimeout();
+            setMessage(message);
+            setMessageTimeout(setTimeout(() => {
+                setMessage(null);
+                setMessageTimeout(null);
+            }, 2000));
+        }
+    }, [clearMessageTimeout, game, guessInput]);
+
+    const startNewGame = useCallback(function (): void {
+        game.startNew();
+        setGuessInput([]);
+        setMessage(null);
+        setMessageTimeout(null);
+    }, [game]);
+
+    const handleKeyboardEvent = useCallback(function (ev: KeyboardEvent): void {
+        if (game.gameState !== GameState.InProgress) {
+            if (ev.key === "Enter") {
+                startNewGame();
+            }
+        } else {
+
+            if (message !== null) {
+                clearMessageTimeout();
+                setMessage(null);
+                setMessageTimeout(null);
+            }
+
+            if (guessInput.length < game.word.length) {
+                if (Game.VALID_LETTERS.includes(ev.key.toUpperCase())) {
+                    const updatedGuessInput: string[] = guessInput.concat(ev.key.toUpperCase());
+                    setGuessInput(updatedGuessInput);
+                }
+            }
+
+            if (ev.key === "Enter") {
+                processGameTurn();
+            }
+
+            if (ev.key === "Backspace") {
+                const updatedGuessInput: string[] = guessInput.slice(0, guessInput.length - 1);
+                setGuessInput(updatedGuessInput);
+            }
+        }
+    }, [clearMessageTimeout, game.gameState, game.word.length, guessInput, message, processGameTurn, startNewGame]);
+
+    useEffect(() => {
+        window.addEventListener('keyup', handleKeyboardEvent);
+
+        return () => {
+            window.removeEventListener('keyup', handleKeyboardEvent);
+        };
+    }, [guessInput, handleKeyboardEvent]);
+
+
+    // function handleKeyboardEvent(ev: KeyboardEvent): void {
+
+    // }
+
+
+    function handleButtonClick(key: string): void {
+        if (game.gameState !== GameState.InProgress) {
+            if (key === "Enter") {
+                startNewGame();
+            }
+        } else {
+            if (key === 'Enter') {
+                processGameTurn();
+            } else if (key === "Back") {
+                const updatedGuessInput: string[] = guessInput.slice(0, guessInput.length - 1);
+                setGuessInput(updatedGuessInput);
+            } else if (guessInput.length < game.word.length) {
+                const updatedGuessInput: string[] = guessInput.concat(key.toUpperCase());
+                setGuessInput(updatedGuessInput);
+            }
+        }
+    }
+
+    const gameContainer = <GameContainer game={game} guessInput={guessInput}
+        keyAction={handleButtonClick} />;
+    if (message === null) {
+        return gameContainer;
+    } else {
+        return <>
+            {gameContainer}
+            <MessageBox message={message}></MessageBox>
+        </>;
+    }
+}
+
+export default Wordle;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Wordle from './Wordle';
 import reportWebVitals from './reportWebVitals';
 import Game from './game';
 import wordList from './resources/wordList';
+import App from './App';
+import { BrowserRouter, Link, Route, Router, Routes } from 'react-router-dom';
 
 const game = new Game(wordList);
+const Home = () => <div><h2>Home</h2></div>;
+const About = () => <div><h2>About Us</h2></div>;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App game={game} />
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/wordle">Wordle</Link>
+          </li>
+        </ul>
+      </div>
+
+      <hr />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/wordle" element={<Wordle game={game} />} />
+      </Routes>
+    </BrowserRouter>
+    {/* <App /> */}
+    {/* <Wordle game={game} /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
