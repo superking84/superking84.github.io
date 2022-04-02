3b0f72a3e337c6ed6665ac05359b24c4d6e14178
Frank Harvey <frankharvey88@gmail.com>
2022-04-02 12:56:47 -0400

    replace custom navbar with bootstrap navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import NavBar from "./components/NavBar/NavBar";
 import { Outlet } from "react-router-dom";
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 function App() {
     return <>
-        <NavBar />
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">Go Home</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/about">About Me</Nav.Link>
+                        <NavDropdown title="Projects" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/wordle">Wordle</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
 
         <Outlet />
     </>;
