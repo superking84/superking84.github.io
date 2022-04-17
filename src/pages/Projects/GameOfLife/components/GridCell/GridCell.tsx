67d4426414a43920bd74ad8b2c673afb5166379e
Frank Harvey <frankharvey88@gmail.com>
2022-04-17 13:40:03 -0400

    added smoother UI controls for cell toggling

--- src/pages/Projects/GameOfLife/components/GridCell/GridCell.tsx
+++ src/pages/Projects/GameOfLife/components/GridCell/GridCell.tsx
@@ -1,14 +1,27 @@
 import "./GridCell.scss";
 
+const MAIN_MOUSE_BUTTON_DOWN = 1;
+
 interface GridCellProps {
     isAlive: boolean;
     rowIndex: number;
     columnIndex: number;
-    toggleCell: (rowIndex: number, columnIndex: number) => void;
+    toggleCell: (rowIndex: number, columnIndex: number, makeAlive?: boolean) => void;
 }
 
 function GridCell({ isAlive, toggleCell, rowIndex, columnIndex }: GridCellProps) {
-    return <div onClick={() => toggleCell(rowIndex, columnIndex)} className={`cell${isAlive ? " alive" : ""}`}>
+    return <div
+        onMouseMove={(e) => e.preventDefault()}
+        onMouseEnter={(e) => {
+            if (e.buttons === MAIN_MOUSE_BUTTON_DOWN) {
+                const makeAlive = !e.ctrlKey;
+                toggleCell(rowIndex, columnIndex, makeAlive);
+            }
+        }}
+        onMouseDown={(e) => {
+            const makeAlive = !e.ctrlKey;
+            toggleCell(rowIndex, columnIndex, makeAlive);
+        }} className={`cell${isAlive ? " alive" : ""}`}>
     </div>;  
 }
 
